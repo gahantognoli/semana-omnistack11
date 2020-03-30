b124f5815ce9ade74d8a8d82aff947aded761f4b
gantognoli <gabriel_antognoli@hotmail.com>
2020-03-30 17:34:00 -0300

    Adicionando validações e testes ao projeto

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development);
+const env = process.env.NODE_ENV == 'test' ? configuration.teste : configuration.development;
+
+const connection = knex(env);
 
 module.exports = connection;
